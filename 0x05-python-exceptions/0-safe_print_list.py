96285ab672fc063ff7ec30f8b5eecdebd14ac174
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-12-18 19:57:49 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     n = 0
     try:
@@ -9,4 +10,4 @@ def safe_print_list(my_list=[], x=0):
         return x
     except IndexError:
         print("")
-        return n
+        return (n)
