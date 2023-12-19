dbc29fc09c1df4888e4d2c0f9e96e4217258d6b2
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-12-18 20:16:07 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 import sys
-
-
 def safe_function(fct, *args):
     try:
         function = fct(*args)
         return(function)
     except Exception as err:
         sys.stderr.write('Exception: {}\n'.format(err))
-        return None
+        result = None
+        return (result)
