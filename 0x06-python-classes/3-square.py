2da2379bbbeed6d55cf3a53fdadc744351ba4b9f
ChilledzoneTechnologies <narteytettehe@gmail.com>
2023-12-19 21:57:10 -0800

    100-singly_linked_list.p

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""My first square"""
-
-
 class Square:
-    """Class define a square"""
-    __size = ''
-
+    """Write a class Square that defines a square"""
     def __init__(self, size=0):
-        """init"""
-        self.__size = size
+        """Module __init__ for size square validation"""
         if type(size) is not int:
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        """Instance that return current square area"""
-        return(self.__size * self.__size)
+        """Method area return the square Area"""
+        return(self.__size ** 2)
