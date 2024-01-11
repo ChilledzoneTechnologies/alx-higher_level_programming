6d22f6e30a402c542367770c563e7b7168e3e59c
ChilledzoneTechnologies <narteytettehe@gmail.com>
2024-01-10 19:01:21 -0800

    101-lazy_matrix_mul.txt

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
-"""
-Text indentation
-"""
+# 5-text_indentation.py
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
+    """Print text with two new lines after each '.', '?', and ':'.
+
+    Args:
+        text (string): The text to print.
+    Raises:
+        TypeError: If text is not a string.
     """
-    function that prints a text with 2 new lines after
-    each of these characters: ., ? and :
-    """
-    if type(text) is not str:
-        raise TypeError('text must be a string')
-    else:
-        for l in range(len(text)):
-            if text[l] == '.' or text[l] == '?' or text[l] == ':':
-                print(text[l], end='\n')
-                print()
-            elif text[l] == ' ' and (text[l - 1] == '.' or
-                                     text[l - 1] == '?' or text[l - 1] == ':'):
-                continue
-            else:
-                print(text[l], end='')
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
